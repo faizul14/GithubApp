6a3134f79b58ebee71b8326b9a077cb470e558c7
faizul14 <fpcyber61@gmail.com>
2023-01-07 07:37:53 +0800

    unit test done

--- app/src/main/java/com/example/githubapp/core/utils/DateUtils.kt
+++ app/src/main/java/com/example/githubapp/core/utils/DateUtils.kt
@@ -6,15 +6,43 @@ import java.util.concurrent.TimeUnit
 
 object DateUtils {
 
-    fun getDateUpdate(time: String):String{
-        val pastTimeString = "2023-01-05T07:11:50Z"
+    fun getDateUpdate(pastTimeString: String): String {
+        //format 2023-01-05T07:11:50Z
+        val currentTimeDumy = 1673046708402
         val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
         dateFormat.timeZone = TimeZone.getTimeZone("UTC")
         val pastTimeDate = dateFormat.parse(pastTimeString)
         val pastTime = pastTimeDate.time
         val currentTime = System.currentTimeMillis()
-        val timeDifference = currentTime - pastTime
+        val timeDifference = currentTimeDumy - pastTime
         val seconds = TimeUnit.MILLISECONDS.toSeconds(timeDifference)
-        return seconds.toString()
+        return getUpdateDetails(seconds.toInt())
+    }
+
+    fun getUpdateDetails(time: Int): String {
+        var data = ""
+        var temp = 0
+        if (time > 604800) {
+            //minggu
+            temp = time / 604800
+            data = "$temp weeks ago"
+        } else if (time > 86400) {
+            //hari
+            temp = time / 86400
+            data = "$temp day ago"
+        } else if (time > 3600) {
+            //jam
+            temp = time / 3600
+            data = "$temp hours ago"
+        } else if (time > 60) {
+            //menit
+            temp = time / 60
+            data = "$temp minute ago"
+        } else {
+            //menit
+            temp = time
+            data = "$temp seconds ago"
+        }
+        return data
     }
 }
--- app/src/test/java/com/example/githubapp/core/utils/DateUtilsTest.kt
+++ app/src/test/java/com/example/githubapp/core/utils/DateUtilsTest.kt
@@ -1,5 +1,21 @@
 package com.example.githubapp.core.utils
 
+import org.junit.Assert
 import org.junit.Assert.*
+import org.junit.Test
 
-class DateUtilsTest
+class DateUtilsTest{
+
+    @Test
+    fun `get correct date lastUpdate`(){
+        val pastTime = "2023-01-05T07:11:50Z"
+        Assert.assertEquals("1 day ago", DateUtils.getDateUpdate(pastTime))
+    }
+
+    @Test
+    fun `get correct date lastUpdateDetails`(){
+        Assert.assertEquals("1 day ago", DateUtils.getUpdateDetails(143998))
+    }
+
+
+}
